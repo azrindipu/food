0f6006327d4eb759804b2e54d2a46862ab41ea89
Golam Azrin <golam.azrin@dsinnovators.com>
2020-08-16 17:44:53 +0600

    added swagger

---
+++ src/main/java/com/azrin/food/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.azrin.food.utils;
+
+public class Constants {
+    public static final String BASE_API_URL="api/food/v1/";
+    public static final String API_BASE_PACKAGE="com.azrin.food.controller";
+
+    /*------------------------------- API Response Style ------------------------------*/
+    public static final String RESPONSE_BODY_DATA = "data";
+    public static final String RESPONSE_BODY_PAGEINFO = "pageInfo";
+    public static final String RESPONSE_BODY_STATUS = "status";
+    public static final String RESPONSE_BODY_MESSAGE = "message";
+    public static final String MESSAGE_SUCCESSFUL = "successful";
+}
---
+++ src/main/java/com/azrin/food/utils/SwaggerValues.java
@@ -0,0 +1,16 @@
+package com.azrin.food.utils;
+
+public class SwaggerValues {
+
+    /*----------------------------- swagger status ------------------------*/
+    public static final String STATUS_OK = "OK";
+    public static final String STATUS_BAD_REQUEST = "Bad Request";
+    public static final String STATUS_INTERNAL_SERVER_ERROR = "Internal Server Error";
+    public static final String STATUS_NOT_FOUND = "Not Found";
+
+    /*----------------------------- test controller ------------------------*/
+    public static final String TEST_CONTROLLER_VALUE = "Api Swagger Test";
+    public static final String TEST_CONTROLLER_TEST_VALUE = "Test Get Request Value";
+    public static final String TEST_CONTROLLER_TEST_NOTES = "Test Get Request Notes";
+    public static final int TEST_CONTROLLER_TEST_POSSITION = 1;
+}
